46ebe42f3285357c65fd9987841779d12858f76c
Jesus <alu0100904932@ull.edu.es>
2018-11-06 14:25:05 +0000

    Modificado Rakefile y añadidas expectativas para la inicializacion del objeto y el acceso a sus atributos

--- spec/prct06_spec.rb
+++ spec/prct06_spec.rb
@@ -1,11 +1,34 @@
 require "spec_helper"
+require 'lib/prct06'
 
 RSpec.describe Prct06 do
-  it "has a version number" do
-    expect(Prct06::VERSION).not_to be nil
+  
+  before :each do
+    @alimento1 = InformacionNutricional.new("Alimento 1", 10, 20, 30, 40, 50, 60)
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe "El alimento se inicializa correctamente y se puede acceder a sus atributos" do
+    it "Se almacena y se accede correctamente al atributo nombre" do
+      expect(@alimento1.nombre).to eq("Alimento 1")
+    end
+    it "Se almacena y se accede correctamente al atributo grasas" do
+      expect(@alimento1.grasas).to eq(10)
+    end
+    it "Se almacena y se accede correctamente al atributo grasasSat" do
+      expect(@alimento1.grasasSat).to eq(20)
+    end 
+    it "Se almacena y se accede correctamente al atributo hidratos" do
+      expect(@alimento1.hidratos).to eq(30)
+    end
+    it "Se almacena y se accede correctamente al atributo azucares" do
+      expect(@alimento1.azucares).to eq(40)
+    end
+    it "Se almacena y se accede correctamente al atributo proteinas" do
+      expect(@alimento1.proteinas).to eq(50)
+    end
+    it "Se almacena y se accede correctamente al atributo nombre" do
+      expect(@alimento1.sal).to eq(60)
+    end
   end
+  
 end
