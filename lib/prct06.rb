77ae1637138d63edc149ae1bb013281acfdf14d4
Jesus <alu0100904932@ull.edu.es>
2018-11-13 10:19:55 +0000

    Solucionando problema con el arbol de directorios

--- lib/prct06.rb
+++ lib/prct06.rb
@@ -1,5 +1,33 @@
 require "prct06/version"
 
-module Prct06
-  # Your code goes here...
+class InformacionNutricional
+    attr_reader :nombre, :grasas, :grasasSat, :hidratos, :azucares, :proteinas, :sal
+    
+    def initialize (nombre, grasas, grasasSat, hidratos, azucares, proteinas, sal)
+        @nombre = nombre
+        @grasas = grasas
+        @grasasSat = grasasSat
+        @hidratos = hidratos
+        @azucares = azucares
+        @proteinas = proteinas
+        @sal = sal
+    end
+    
+    def valorEnergeticoKJ
+        (@grasas * 37) + (@hidratos * 17) + (@proteinas * 17)
+    end
+    
+    def valorEnergeticoKcal
+        (@grasas * 9) + (@hidratos * 4) + (@proteinas * 4)
+    end
+    
+    def to_s
+        "Nombre: #{@nombre}\n" +
+        "Grasas: #{@grasas}\n" +
+        "Grasas saturadas: #{@grasasSat}\n" +
+        "Hidratos: #{@hidratos}\n" +
+        "Azucares:  #{@azucares}\n" +
+        "Proteinas: #{@proteinas}\n" +
+        "Sal: #{@sal}"
+    end
 end
--- spec/prct06_spec.rb
+++ spec/prct06_spec.rb
@@ -26,8 +26,29 @@ RSpec.describe Prct06 do
     it "Se almacena y se accede correctamente al atributo proteinas" do
       expect(@alimento1.proteinas).to eq(50)
     end
-    it "Se almacena y se accede correctamente al atributo nombre" do
+    it "Se almacena y se accede correctamente al atributo sal" do
       expect(@alimento1.sal).to eq(60)
+    end
+  end
+  
+  describe "Calculo del valor energetico" do
+    it "Se calcula el valor energetico en KJ correctamente" do
+      expect(@alimento1.valorEnergeticoKJ).to eq(1730)
+    end
+    it "Se calcula el valor energetico en kcal correctamente" do
+      expect(@alimento1.valorEnergeticoKcal).to eq(410)
+    end
+  end
+  
+  describe "Formato de la informacion" do
+    it "Se muestra la informacion de forma correcta" do
+      expect(@alimento1.to_s).to eq("Nombre: #{@alimento1.nombre}\n" +
+                                    "Grasas: #{@alimento1.grasas}\n" +
+                                    "Grasas saturadas: #{@alimento1.grasasSat}\n" +
+                                    "Hidratos: #{@alimento1.hidratos}\n" +
+                                    "Azucares:  #{@alimento1.azucares}\n" +
+                                    "Proteinas: #{@alimento1.proteinas}\n" +
+                                    "Sal: #{@alimento1.sal}")
     end
   end
   
